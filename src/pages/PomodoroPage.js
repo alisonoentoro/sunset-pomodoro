163cd43a825c819150798114077716998f7fd660
Alison Oentoro <51717872+alisonoentoro@users.noreply.github.com>
2026-01-07 13:14:00 -0500

    Remove unused useMemo import in PomodoroPage

--- src/pages/PomodoroPage.js
+++ src/pages/PomodoroPage.js
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState } from "react";
 import Pomodoro from "../component/Pomodoro";
 import Dropdown from "../component/Dropdown"; 
 import GradientBackground from "../component/GradientBackground";
